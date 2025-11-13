854f105842ac3ae6d2d60e4c17dc498df1b0fc9f
Lucas Alves <lucas1901alves@gmail.com>
2025-11-13 17:25:50 -0300

    feat(findAll): UserController

--- backend/src/main/java/com/alvesdev/CertiLink/controller/UserController.java
+++ backend/src/main/java/com/alvesdev/CertiLink/controller/UserController.java
@@ -3,20 +3,30 @@ package com.alvesdev.CertiLink.controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.alvesdev.CertiLink.model.entity.Users.User;
+import com.alvesdev.CertiLink.model.dto.UserResponseDTO;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-
+import com.alvesdev.CertiLink.repository.UserRepository;
 @RestController
-@RequestMapping("/users")
+@RequestMapping("users")
 public class UserController {
 
+    @Autowired
+    private UserRepository userRepository;
 
-    @GetMapping("path")
-    public ResponseEntity<User> getAllUsers(@RequestParam String param) {
-        return ResponseEntity.ok(new User());
+    @GetMapping("/all")
+    public ResponseEntity<List<UserResponseDTO>> getAllUsers() {
+
+        List<UserResponseDTO> allUsers = userRepository.findAll().stream()
+                .map(UserResponseDTO::new)
+                .toList();
+
+        return ResponseEntity.ok(allUsers);
     }
     
     
