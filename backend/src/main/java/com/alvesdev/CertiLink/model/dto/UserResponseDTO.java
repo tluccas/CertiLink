c82534e09f5830fbb949c47eb5cca291de4ab666
Lucas Alves <lucas1901alves@gmail.com>
2025-11-13 17:25:18 -0300

    feat(DTO): UserResponseDTO

---
+++ backend/src/main/java/com/alvesdev/CertiLink/model/dto/UserResponseDTO.java
@@ -0,0 +1,15 @@
+package com.alvesdev.CertiLink.model.dto;
+
+import com.alvesdev.CertiLink.model.entity.Users.User;
+
+public record UserResponseDTO(Long id, String name, String email, String role) {
+
+    public UserResponseDTO(User user){
+        this(
+            user.getId(),
+            user.getUsername(),
+            user.getEmail(),
+            user.getRole().getName()
+        );
+    }
+}
