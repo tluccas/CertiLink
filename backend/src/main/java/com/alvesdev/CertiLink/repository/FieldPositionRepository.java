450812072207dbb6e7898124a67b9f481681c24a
Lucas Alves <lucas1901alves@gmail.com>
2025-11-14 02:44:08 -0300

    feat(findBy): method findByTemplateId

--- backend/src/main/java/com/alvesdev/CertiLink/repository/FieldPositionRepository.java
+++ backend/src/main/java/com/alvesdev/CertiLink/repository/FieldPositionRepository.java
@@ -1,8 +1,10 @@
 package com.alvesdev.CertiLink.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.alvesdev.CertiLink.model.entity.Certificado.*;
 
 public interface FieldPositionRepository extends JpaRepository<FieldPosition, Long> {
-
+    Optional<FieldPosition> findByTemplateId(Long templateId);
 } 
